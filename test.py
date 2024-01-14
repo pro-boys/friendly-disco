c47d83d4135fc0c4ee8a94705955a50249756d74
Anand P S <62738084+anandpskerala@users.noreply.github.com>
2024-01-14 02:10:12 -0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+from ultralytics import YOLO
+
+# Create a new YOLO model from scratch
+model = YOLO('yolov8n.yaml')
+
+# Load a pretrained YOLO model (recommended for training)
+model = YOLO('yolov8n.pt')
+
+# Train the model using the 'coco128.yaml' dataset for 3 epochs
+results = model.train(data='coco128.yaml', epochs=3)
+
+# Export the model to ONNX format
+success = model.export(format='onnx')
